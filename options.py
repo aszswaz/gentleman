8495b2f4d3724313ca008e23ff452d49e4363e86
朽木不可雕也 <aszswaz@163.com>
2022-08-05 23:15:44 +0800

    添加 output 校验

--- options.py
+++ options.py
@@ -1,5 +1,5 @@
 from argparse import ArgumentParser
-from os import getcwd
+import os
 
 
 class Options:
@@ -12,19 +12,26 @@ class Options:
         arg_parse = ArgumentParser(description="bilibili video vdownload")
         arg_parse.add_argument("urls", metavar="URL", type=str, nargs="+", help="video url.")
         arg_parse.add_argument("--cookie", type=str, required=True, help="account cookies.")
-        arg_parse.add_argument("--filename-reg", type=str, help="Extract the filename from the video's title via a regular expression.")
-        arg_parse.add_argument("--output", type=str, help="video save path, the default is working directory.")
+        arg_parse.add_argument(
+            "--filename-reg", type=str,
+            help="Extract the filename from the video's title via a regular expression.")
+        arg_parse.add_argument("--output", type=str, default=os.getcwd(),
+                               help="video save path, the default is working directory.")
         args = arg_parse.parse_args()
 
         self.urls = args.urls
         self.cookie = args.cookie
         self.filename_reg = args.filename_reg
-        if args.output is None:
-            self.output = getcwd()
-        elif args.output.endswith("/"):
-            self.output = args.output[0: len(self.output) - 1]
+        self.output = args.output.endswith("/") and args.output[0: len(self.output) - 1] or args.output
+
+        # 确保程序具有对指定文件夹的写入权限，如果文件夹不存在则自动创建文件夹
+        if os.path.exists(self.output):
+            if not os.path.isdir(self.output):
+                raise Exception(f"not a directory: {self.output}")
+            if not os.access(self.output, os.W_OK):
+                raise Exception(f"Permission denied: {self.output}")
         else:
-            self.output = args.output
+            os.makedirs(self.output)
 
     def __repr__(self):
         return self.__dict__.__repr__()
